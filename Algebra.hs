29d26b125b64f18c7031e56f05518324c44b2b04
Felipe de Morais <felipejpa15@gmail.com>
2014-12-06 13:26:25 -0200

    trying to use class and instance in the Algebra module

--- Algebra.hs
+++ Algebra.hs
@@ -2,24 +2,33 @@
 module Algebra where
 
 -- Aqui vem a definição das entidades algébricas que precisarão ser utilizadas
-data Vector3D = Coord (Double, Double, Double)
+data Vector3D = Coord (Double,  Double,  Double)
               | Color (Integer, Integer, Integer)
     deriving (Show, Eq)
 
-($+) :: Vector3D -> Vector3D -> Vector3D
-($+) (Coord (a, b, c)) (Coord (d, e, f)) = Coord (a + d, b + e, c + f)
+class Vector a where
+    ($+)      :: Vector3D -> Vector3D -> Vector3D
+    ($-)      :: Vector3D -> Vector3D -> Vector3D
+    ($*)      :: Double   -> Vector3D -> Vector3D
+    ($.)      :: Vector3D -> Vector3D -> Double
+    ($/)      :: Vector3D -> Double   -> Vector3D
+    normalize :: Vector3D -> Vector3D
 
-($-) :: Vector3D -> Vector3D -> Vector3D
-($-) x (Coord (a, b, c)) = x $+ (Coord (-a, -b, -c))
+instance Vector Vector3D where
+    ($+) :: Vector3D -> Vector3D -> Vector3D
+    ($+) (Coord (a, b, c)) (Coord (d, e, f)) = Coord (a + d, b + e, c + f)
 
-($*) :: Double -> Vector3D -> Vector3D
-($*) x (Coord (a, b, c)) = Coord (x * a, x * b, x * c)
+    ($-) :: Vector3D -> Vector3D -> Vector3D
+    ($-) x (Coord (a, b, c)) = x $+ (Coord (-a, -b, -c))
 
-($.) :: Vector3D -> Vector3D -> Double
-($.) (Coord (a, b, c)) (Coord (d, e, f)) = a * d + b * e + c * f
+    ($*) :: Double -> Vector3D -> Vector3D
+    ($*) x (Coord (a, b, c)) = Coord (x * a, x * b, x * c)
 
-($/) :: Vector3D -> Double -> Vector3D
-($/) x y = (1 / y) $* x
+    ($.) :: Vector3D -> Vector3D -> Double
+    ($.) (Coord (a, b, c)) (Coord (d, e, f)) = a * d + b * e + c * f
 
-normalize :: Vector3D -> Vector3D
-normalize x = x $/ (x $. x)
+    ($/) :: Vector3D -> Double -> Vector3D
+    ($/) x y = (1 / y) $* x
+
+    normalize :: Vector3D -> Vector3D
+    normalize x = x $/ (x $. x)
